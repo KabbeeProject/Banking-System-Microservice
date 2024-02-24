7f2733b0802324dd30a95aef95bd667e85ce9ba9
MusieNew <mussieg2019@gmail.com>
2024-02-23 18:08:44 -0800

    first commit

---
+++ bright/src/test/java/BankSystem/bright/BrightApplicationTests.java
@@ -0,0 +1,13 @@
+package BankSystem.bright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
