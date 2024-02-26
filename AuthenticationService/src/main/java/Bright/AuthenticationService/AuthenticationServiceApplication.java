eb38630e5ac4d0ef14624560607bdd6bdc73d1a0
MusieNew <mussieg2019@gmail.com>
2024-02-25 21:15:38 -0800

    new brght project

---
+++ AuthenticationService/src/main/java/Bright/AuthenticationService/AuthenticationServiceApplication.java
@@ -0,0 +1,14 @@
+package Bright.AuthenticationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceApplication.class, args);
+		System.out.println("Authenticatiobn service is running");
+	}
+
+}
